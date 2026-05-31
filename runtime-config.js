1c72c5d839e39114dffa3116a55f7904f31fb4c2
gabrielvanbernardino <gabrielivanbernardino@gmail.com>
2026-05-31 22:51:43 +0800

    Add files via upload

---
+++ runtime-config.js
@@ -0,0 +1,6 @@
+window.CEDOC_RUNTIME_CONFIG = {
+  "apiUrl": "",
+  "apiKey": "",
+  "supabaseUrl": "https://ikaunnbriefsgvojcsif.supabase.co",
+  "supabasePublishableKey": "sb_publishable_mUbT5iYQklVshVRghEunyQ_gUoK8FH_"
+};
