ed4fe3f00201877f32152463aea0e48b6f61b229
gabrielvanbernardino <gabrielivanbernardino@gmail.com>
2026-06-05 19:15:51 +0800

    update

--- runtime-config.js
+++ runtime-config.js
@@ -1,6 +1,6 @@
 window.CEDOC_RUNTIME_CONFIG = {
   "apiUrl": "",
   "apiKey": "",
-  "supabaseUrl": "https://ikaunnbriefsgvojcsif.supabase.co",
-  "supabasePublishableKey": "sb_publishable_mUbT5iYQklVshVRghEunyQ_gUoK8FH_"
+  "supabaseUrl": "https://sxznmwevxluzwbmtjljl.supabase.co",
+  "supabasePublishableKey": "sb_publishable_HkfbvTbsm5LdepxcGuNm_Q_ok9E_tI-"
 };
